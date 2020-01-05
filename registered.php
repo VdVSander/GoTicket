61230c3e95aefd9417069b61256b7234d1aef0f2
VdVSander <sander.vandeven86@gmail.com>
2020-01-05 16:30:41 +0100

    Just a php test Sander

--- registered.php
+++ registered.php
@@ -47,8 +47,9 @@
     <div class="container">
       <?php
         echo "<p class='para-1'> Je hebt je bedrijf: ";
-        echo $_POST['bedrijfsnaam']; 
+        echo $_POST['bedrijfsnaam'];
         echo " succesvol geregistreerd! <br> We houden je op de hoogte van verdere ontwikkelingen. </p>";
+        phpinfo();
       ?>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
