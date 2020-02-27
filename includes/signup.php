ffe2d4a5378c3ecf409fb56bc43f3005b4251475
VdVSander <sander.vandeven86@gmail.com>
2020-02-27 15:08:37 +0100

    Pffttt... php werkt ni

--- includes/signup.php
+++ includes/signup.php
@@ -1,26 +1,31 @@
 <?php
+  echo "-- TEST START TEST --";
   if(isset($_POST['signup-submit']))
   {
+    echo "Subit knop ingedrukt";
     require 'config.php';
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password']
 
-    $sql = "SELECT naam FROM organisaties WHERE naam=?";
+    $sql = "SELECT * FROM organisaties WHERE email=?;";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql))
     {
+      echo "ERROR: SQL 1";
       header("Location: ../register.php?error=sqlerror");
       exit();
     }
     else
     {
-      mysqli_stmt_bind_param($stmt, "s", $name);
+      mysqli_stmt_bind_param($stmt, "s", $email);
+      echo "SQL STMT 1: $stmt";
       mysqli_stmt_execute($stmt);
       mysqli_stmt_store_result($stmt);
       $resultcheck = mysqli_stmt_num_rows($stmt);
       if($resultcheck > 0)
       {
+        echo "ERROR: Email $email bestaat al";
         header("Location: ../register.php?error=userexists&email=".$email);
         exit();
       }
@@ -30,13 +35,14 @@
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql))
         {
+          echo "ERROR: SQL 2";
           header("Location: ../register.php?error=sqlerror");
           exit();
         }
         else
         {
           $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
-
+          echo "Hashed pwd: $passwordHashed";
           mysqli_stmt_bind_param($stmt, "sss", $name, $email, $passwordHashed);
           mysqli_stmt_execute($stmt);
           header("Location: ../registered.html");
@@ -44,7 +50,7 @@
         }
       }
     }
-
+    echo "SQL conn sluiten";
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
   }
