3b7cc11b5cd6f7f3416032ffda894a374d76f395
VdVSander <sander.vandeven86@gmail.com>
2020-03-02 23:10:38 +0100

    PHP werkt, kinda... Sander

--- includes/signup.php
+++ includes/signup.php
@@ -1,33 +1,31 @@
 <?php
-  echo "-- TEST START TEST --";
+  echo '-- TEST START TEST --';
   if(isset($_POST['signup-submit']))
   {
-    echo "Subit knop ingedrukt";
+    echo 'Subit knop ingedrukt';
     require 'config.php';
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $password = $_POST['password']
+    $password = $_POST['password'];
 
     $sql = "SELECT * FROM organisaties WHERE email=?;";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql))
     {
-      echo "ERROR: SQL 1";
+      echo 'ERROR: SQL 1';
       header("Location: ../register.php?error=sqlerror");
-      exit();
     }
     else
     {
       mysqli_stmt_bind_param($stmt, "s", $email);
-      echo "SQL STMT 1: $stmt";
+      echo 'SQL STMT 1: $stmt';
       mysqli_stmt_execute($stmt);
       mysqli_stmt_store_result($stmt);
       $resultcheck = mysqli_stmt_num_rows($stmt);
       if($resultcheck > 0)
       {
-        echo "ERROR: Email $email bestaat al";
+        echo 'ERROR: Email $email bestaat al';
         header("Location: ../register.php?error=userexists&email=".$email);
-        exit();
       }
       else
       {
@@ -35,28 +33,25 @@
         $stmt = mysqli_stmt_init($conn);
         if(!mysqli_stmt_prepare($stmt, $sql))
         {
-          echo "ERROR: SQL 2";
+          echo 'ERROR: SQL 2';
           header("Location: ../register.php?error=sqlerror");
-          exit();
         }
         else
         {
           $passwordHashed = password_hash($password, PASSWORD_DEFAULT);
-          echo "Hashed pwd: $passwordHashed";
+          echo 'Hashed pwd: $passwordHashed';
           mysqli_stmt_bind_param($stmt, "sss", $name, $email, $passwordHashed);
           mysqli_stmt_execute($stmt);
           header("Location: ../registered.html");
-          exit();
         }
       }
     }
-    echo "SQL conn sluiten";
+    echo 'SQL conn sluiten';
     mysqli_stmt_close($stmt);
     mysqli_close($conn);
   }
   else
   {
     header("Location: ../register.php?error=button_not_pressed");
-    exit();
   }
 ?>
