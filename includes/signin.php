3943828db6404e7e0ebc0f274d1fe4ce8bc91c5f
VdVSander <sander.vandeven86@gmail.com>
2020-04-15 13:32:06 +0200

    update signin php

--- includes/signin.php
+++ includes/signin.php
@@ -43,8 +43,40 @@
         }
         else
         {
-          header("Location: ../login.php?error=nouser");
-        }
+          $sql = "SELECT * FROM klanten WHERE email=?;";
+          $stmt = mysqli_stmt_init($conn);
+          if(!mysqli_stmt_prepare($stmt, $sql))
+          {
+            header("Location: ../login.php?error=SQL_error2");
+          }
+          else
+          {
+            mysqli_stmt_bind_param($stmt, "s", $mail);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if($row = mysqli_fetch_assoc($result))
+            {
+              $pwdCheck = password_verify($password, $row['wachtwoord']);
+              if($pwdCheck == false)
+              {
+                header("Location: ../login.php?error=wrongpassword");
+              }
+              else if ($pwdCheck == true)
+              {
+                session_start();
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['klant'] = $row['voornaam'];
+                header("Location: ../loggedin.php");
+              }
+              else
+              {
+                header("Location: ../login.php?error=pwdisnopwd");
+              }
+            }
+            else
+            {
+              header("Location: ../login.php?error=nouser");
+            }
       }
     }
   }
