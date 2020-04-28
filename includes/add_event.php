3f79ed14acfc2a971141ed563d638737ad01d7d2
VdVSander <sander.vandeven86@gmail.com>
2020-04-28 14:55:54 +0200

    Update nieuw evenement. Sander

--- includes/add_event.php
+++ includes/add_event.php
@@ -9,26 +9,27 @@
     $location = $_POST['location'];
     $startdate = $_POST['start_date'];
     $stopdate = $_POST['stop_date'];
-    $organisatieid = $_SESSION['organisatieid'];
+    $organisatieid = $_SESSION['organisatieID'];
 
     $sql = "INSERT INTO evenementen (naam,adres,stad,postcode,startdatum,stopdatum,organisaties_organisatieid,locatienaam) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql))
-      {
-        header("Location: ../add_new_event.php?error=sqlerror");
-      }
-      else
-      {
-        mysqli_stmt_bind_param($stmt, "ssssssss", $eventname, $adress, $city, $postalcode, $startdate, $stopdate, $organisatieid, $location);
-        mysqli_stmt_execute($stmt);
-        $message = $eventname." is een nieuw evenement op de website.";
-        mail($to_mail,$subject_reg,$message);
-        header("Location: ../dashboard_add_event_success.php");
-      }
-      mysqli_stmt_close($stmt);
-      mysqli_close($conn);
+    {
+      header("Location: ../register.php?error=sqlerror");
+    }
+    else
+    {
+      mysqli_stmt_bind_param($stmt, "ssssssss", $eventname, $adress, $city, $postalcode, $startdate, $stopdate, $organisatieid, $location);
+      mysqli_stmt_execute($stmt);
+      $message = $eventname." is een nieuw evenement op de website!";
+      mail($to_mail,$subject_reg,$message);
+      header("Location: ../add_new_event.php?success");
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+  }
   else
   {
-    header("Location: ../new_event.php?error=btn_not_pressed");
+    header("Location: ../register.php?error=button_not_pressed");
   }
  ?>
